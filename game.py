01a3f1d00cc201eef4f9ebd86157ea4ece46fe5b
Alex <ahowe23@my.bcit.ca>
2024-11-12 18:31:40 -0800

    Implement make_character

--- game.py
+++ game.py
@@ -4,6 +4,7 @@ A01309729
 """
 
 import random
+from locale import format_string
 
 
 def make_board(rows, columns):
@@ -16,26 +17,55 @@ def make_board(rows, columns):
                 board[coordinates] = "Safe area"
             else:
                 board[coordinates] = "Wild grass"
+            board[2, 2] = "Pokecenter"
 
     return board
 
 
 def make_character():
-    pass
+    fire = "Charmander"
+    water = "Squirtle"
+    grass = "Bulbasaur"
+    choice = (input("You get a choice between 3 unique Pokemon on your journey.\nThere is Charmander: the fire type "
+                    "Pokemon, Squirtle: the water type Pokemon, and Bulbasaur: the grass type Pokemon.\nFire is weak "
+                    "to water and strong against grass. Grass is weak to fire and strong against water. Water is weak "
+                    "to grass and strong against fire.\nFor example, water attacks will do less damage to grass "
+                    "Pokemon, but more damage to fire Pokemon.\nYou will not be able to change your selection. You "
+                    "will start with 50 HP and the game will end if your Pokemon loses all of their health.\nEnter 1 "
+                    "to choose Charmander, 2 to choose Squirtle, or 3 to choose Bulbasaur. "))
+    while choice not in ["1", "2", "3"]:
+        print("This is not a valid selection.\n")
+        choice = (input("Enter 1 to choose Charmander, 2 to choose Squirtle, or 3 to choose Bulbasaur. "))
+    if choice == "1":
+        print(f"Congratulations! You selected {fire}, the fire type Pokemon.")
+        character = {"X-coordinate": 0, "Y-coordinate": 0, "Current HP": 50, "First move": "Scratch", "Second move":
+                     "none", "Third move": "none"}
+        return character
+    elif choice == "2":
+        print(f"Congratulations! You selected {water}, the water type Pokemon.")
+        character = {"X-coordinate": 0, "Y-coordinate": 0, "Current HP": 50, "First move": "Bite", "Second move":
+                     "none", "Third move": "none"}
+        return character
+    elif choice == "3":
+        print(f"Congratulations! You selected {grass}, the grass type Pokemon.")
+        character = {"X-coordinate": 0, "Y-coordinate": 0, "Current HP": 50, "First move": "Tackle", "Second move":
+                     "none", "Third move": "none"}
+        return character
 
 
 def game():
-    print("Welcome to the world of Pokemon! You are a brand new pokemon trainer who finds himself in Pallet Town.\n"
+    print("Welcome to the world of Pokemon! You are a brand new Pokemon trainer who finds themself in Pallet Town.\n"
           "Your task is to defeat the local Pokemon master to become the best trainer in town.\nTo do this, you must "
           "select the Pokemon that you want to control.\nYour Pokemon will start at level 1 and will evolve when you "
           "reach levels 2 and 3.\nOnce you reach level 3, you can challenge the Pokemon master by progressing to the "
-          "bottom right of the board.\nEach space on the board has been randomly designated as a Safe area or contains "
-          "Wild grass.\nWild grass contains wild Pokemon that can be defeated for experience, which helps you level.\n"
-          "In the center of the board is a PokeCenter. Stepping on this space will replenish your Pokemon to full "
-          "health.\nYou will learn more in time. For now, it's time to select your Pokemon!\n\n")
+          "bottom right of the board.\nEach space on the board has been randomly designated as a safe area or contains "
+          "wild grass.\nWild grass contains wild Pokemon that can be defeated for experience, which helps you level "
+          "up.\nIn the center of the board is a PokeCenter. Stepping on this space will replenish your Pokemon to full "
+          "health.\nYou will learn more in time. For now, it's time to select your Pokemon!\n")
     rows = 5
     columns = 5
     board = make_board(rows, columns)
+    character = make_character()
 
 
 def main():
