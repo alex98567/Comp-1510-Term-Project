82ee469d6894163644afeac64e74bd877053dd46
Alex <ahowe23@my.bcit.ca>
2024-11-26 21:57:55 -0800

    Add suite of unit tests for test_check_if_ready_for_final_boss

---
+++ test_check_if_ready_for_final_boss.py
@@ -0,0 +1,36 @@
+from unittest import TestCase
+from game import check_if_ready_for_final_boss
+
+
+class Test(TestCase):
+    def test_check_if_ready_for_final_boss_False_2_2(self):
+        actual = check_if_ready_for_final_boss({'X-coordinate': 2, 'Y-coordinate': 2, 'Current HP': 50,
+                                                'Current XP': 0, 'First move': 'Scratch', 'Second move': 'none',
+                                                'Third move': 'none', 'Level': 1, 'Pokemon': 'Charmander'},
+                                               5, 5)
+        expected = False
+        self.assertEqual(actual, expected)
+
+    def test_check_if_ready_for_final_boss_True_2_2(self):
+        actual = check_if_ready_for_final_boss({'X-coordinate': 1, 'Y-coordinate': 1, 'Current HP': 150,
+                                                'Current XP': 500, 'First move': 'Scratch', 'Second move': 'Ember',
+                                                'Third move': 'Fire blast', 'Level': 3, 'Pokemon': 'Charizard'},
+                                               2, 2)
+        expected = True
+        self.assertEqual(actual, expected)
+
+    def test_check_if_ready_for_final_boss_False_4_3(self):
+        actual = check_if_ready_for_final_boss({'X-coordinate': 4, 'Y-coordinate': 3, 'Current HP': 100,
+                                                'Current XP': 0, 'First move': 'Scratch', 'Second move': 'Ember',
+                                                'Third move': 'none', 'Level': 2, 'Pokemon': 'Charmeleon'},
+                                               5, 5)
+        expected = False
+        self.assertEqual(actual, expected)
+
+    def test_check_if_ready_for_final_boss_True_5_5(self):
+        actual = check_if_ready_for_final_boss({'X-coordinate': 4, 'Y-coordinate': 4, 'Current HP': 150,
+                                                'Current XP': 500, 'First move': 'Scratch', 'Second move': 'Ember',
+                                                'Third move': 'Fire blast', 'Level': 3, 'Pokemon': 'Charizard'},
+                                               5, 5)
+        expected = True
+        self.assertEqual(actual, expected)
