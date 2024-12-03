92b8937cbf82f26bfc23ae3fc80f9afef2970a49
Alex <ahowe23@my.bcit.ca>
2024-12-02 22:30:15 -0800

    Add patch to test_evolve_level_3

--- test_evolve_level_3.py
+++ test_evolve_level_3.py
@@ -1,9 +1,11 @@
 from unittest import TestCase
 from game import evolve_level_3, is_grass_2, is_fire_2, is_water_2, is_grass_3, is_fire_3, is_water_3
+from unittest.mock import patch
 
 
 class Test(TestCase):
-    def test_evolve_level_3_fire(self):
+    @patch('builtins.input', side_effect=['2'])
+    def test_evolve_level_3_fire(self, _):
         actual = evolve_level_3({'X-coordinate': 3, 'Y-coordinate': 2, 'Current HP': 33, 'Current XP': 400,
                                  'First move': 'Scratch', 'Second move': 'Ember', 'Third move': 'none', 'Level': 2,
                                  'Pokemon': is_fire_2()})
@@ -11,7 +13,8 @@ class Test(TestCase):
                     'Second move': 'Ember', 'Third move': 'Fire blast', 'Level': 3, 'Pokemon': is_fire_3()}
         self.assertEqual(actual, expected)
 
-    def test_evolve_level_3_water(self):
+    @patch('builtins.input', side_effect=['2'])
+    def test_evolve_level_3_water(self, _):
         actual = evolve_level_3({'X-coordinate': 3, 'Y-coordinate': 2, 'Current HP': 33, 'Current XP': 400,
                                  'First move': 'Bite', 'Second move': 'Water gun', 'Third move': 'none', 'Level': 2,
                                  'Pokemon': is_water_2()})
@@ -19,7 +22,8 @@ class Test(TestCase):
                     'Second move': 'Water gun', 'Third move': 'Hydro pump', 'Level': 3, 'Pokemon': is_water_3()}
         self.assertEqual(actual, expected)
 
-    def test_evolve_level_3_grass(self):
+    @patch('builtins.input', side_effect=['2'])
+    def test_evolve_level_3_grass(self, _):
         actual = evolve_level_3({'X-coordinate': 3, 'Y-coordinate': 2, 'Current HP': 33, 'Current XP': 400,
                                  'First move': 'Tackle', 'Second move': 'Vine whip', 'Third move': 'none', 'Level': 1,
                                  'Pokemon': is_grass_2()})
