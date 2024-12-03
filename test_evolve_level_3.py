9cb396eda8c6991f6385f690a6a6588fd3f4b4f2
Alex <ahowe23@my.bcit.ca>
2024-12-02 17:05:14 -0800

    Add suite of unit tests for test_evolve_level_3

---
+++ test_evolve_level_3.py
@@ -0,0 +1,28 @@
+from unittest import TestCase
+from game import evolve_level_3, is_grass_2, is_fire_2, is_water_2, is_grass_3, is_fire_3, is_water_3
+
+
+class Test(TestCase):
+    def test_evolve_level_3_fire(self):
+        actual = evolve_level_3({'X-coordinate': 3, 'Y-coordinate': 2, 'Current HP': 33, 'Current XP': 400,
+                                 'First move': 'Scratch', 'Second move': 'Ember', 'Third move': 'none', 'Level': 2,
+                                 'Pokemon': is_fire_2()})
+        expected = {'X-coordinate': 3, 'Y-coordinate': 2, 'Current HP': 150, 'Current XP': 400, 'First move': 'Scratch',
+                    'Second move': 'Ember', 'Third move': 'Fire blast', 'Level': 3, 'Pokemon': is_fire_3()}
+        self.assertEqual(actual, expected)
+
+    def test_evolve_level_3_water(self):
+        actual = evolve_level_3({'X-coordinate': 3, 'Y-coordinate': 2, 'Current HP': 33, 'Current XP': 400,
+                                 'First move': 'Bite', 'Second move': 'Water gun', 'Third move': 'none', 'Level': 2,
+                                 'Pokemon': is_water_2()})
+        expected = {'X-coordinate': 3, 'Y-coordinate': 2, 'Current HP': 150, 'Current XP': 400, 'First move': 'Bite',
+                    'Second move': 'Water gun', 'Third move': 'Hydro pump', 'Level': 3, 'Pokemon': is_water_3()}
+        self.assertEqual(actual, expected)
+
+    def test_evolve_level_3_grass(self):
+        actual = evolve_level_3({'X-coordinate': 3, 'Y-coordinate': 2, 'Current HP': 33, 'Current XP': 400,
+                                 'First move': 'Tackle', 'Second move': 'Vine whip', 'Third move': 'none', 'Level': 1,
+                                 'Pokemon': is_grass_2()})
+        expected = {'X-coordinate': 3, 'Y-coordinate': 2, 'Current HP': 150, 'Current XP': 400, 'First move': 'Tackle',
+                    'Second move': 'Vine whip', 'Third move': 'Solarbeam', 'Level': 3, 'Pokemon': is_grass_3()}
+        self.assertEqual(actual, expected)
